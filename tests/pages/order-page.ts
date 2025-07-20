179998dc0c1ecf27d64cedd1b00877fb039a5813
Jevgeni <jevgeni.massalov@gmail.com>
2025-07-20 21:18:34 +0300

    Tests for part 2 of lesson 17 have been completed.

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -3,10 +3,21 @@ import { Locator, Page } from '@playwright/test'
 export class OrderPage {
   readonly page: Page
   readonly statusButton: Locator
-  // add more locators here
+  readonly name: Locator
+  readonly phone: Locator
+  readonly comment: Locator
+  readonly orderButton: Locator
+  readonly logoutButton: Locator
+  readonly notificationPopUp: Locator
 
   constructor(page: Page) {
     this.page = page
     this.statusButton = page.getByTestId('openStatusPopup-button')
+    this.name = page.getByTestId('username-input')
+    this.phone = page.getByTestId('phone-input')
+    this.comment = page.getByTestId('comment-input')
+    this.orderButton = page.getByTestId('createOrder-button')
+    this.logoutButton = page.getByTestId('logout-button')
+    this.notificationPopUp = page.getByTestId('orderSuccessfullyCreated-popup')
   }
 }
