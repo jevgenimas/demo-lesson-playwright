c141c5b8f847bc938e56a2213fe8febf5b794007
Jevgeni <jevgeni.massalov@gmail.com>
2025-07-22 21:24:00 +0300

    Lesson 18 tests are done.

--- tests/pages/login-page.ts
+++ tests/pages/login-page.ts
@@ -1,21 +1,21 @@
 import { Locator, Page } from '@playwright/test'
 import { OrderPage } from './order-page'
 import { SERVICE_URL } from '../../config/env-data'
+import { BasePage } from './base-page'
 
-export class LoginPage {
-  readonly page: Page
+export class LoginPage extends BasePage {
   readonly url: string = SERVICE_URL
   readonly signInButton: Locator
   readonly usernameField: Locator
   readonly passwordField: Locator
-  // add more locators here
+  readonly errorMessage: Locator
 
   constructor(page: Page) {
-    this.page = page
+    super(page)
     this.signInButton = page.getByTestId('signIn-button')
     this.usernameField = page.getByTestId('username-input')
     this.passwordField = page.getByTestId('password-input')
-    // continue with the rest of the implementation below
+    this.errorMessage = page.getByTestId('authorizationError-popup')
   }
 
   async open() {
